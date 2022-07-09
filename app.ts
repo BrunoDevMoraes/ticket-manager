87f10efe4611cb469750ff69e215d5665a4206c5
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-07-09 13:35:10 -0300

    feat: add app.ts and server.ts files

---
+++ app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+    this.config();
+    this.routes();
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(accessControl);
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT);
+    console.log(`App listening to port ${PORT}!`)
+  }
+
+  public routes(): void {
+
+  }
+}
+
+export { App };
