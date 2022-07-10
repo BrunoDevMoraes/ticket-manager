05088f55d47818dd48c2c84f759660eda7babcab
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-07-09 22:27:30 -0300

    fix: change seeders passwords to hash

--- seeders/20220709165052-users.js
+++ seeders/20220709165052-users.js
@@ -8,7 +8,7 @@ module.exports = {
         id: 1,
         name: 'John Doe',
         email: 'johndoe@gmail.com',
-        password: 'john123',
+        password: '$2a$04$69bugOzJN.nwPO0QHLLQDuZCGxOOfLUOfJzVlIoZrW.4SLpapHRXi', // john123
         createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
         updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
@@ -16,7 +16,7 @@ module.exports = {
         id: 2,
         name: 'Mary Jane',
         email: 'maryjane@gmail.com',
-        password: 'mary123',
+        password: '$2a$04$C6of1LLSQ//jYUqsfc98g.Ts26S4SwcMnwqIQSZlg3uLoZeM7TUj6', // mary123
         createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
         updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
@@ -24,7 +24,7 @@ module.exports = {
         id: 3,
         name: 'Will Smith',
         email: 'willsmith@gmail.com',
-        password: 'will123',
+        password: '$2a$04$9ij0PGzFrZvVYR2MOdj7Xes49.coTrskMsfDDM21F0Wwzz.4b9U8i', // will123
         createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
         updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
       }
