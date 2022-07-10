69cb8a26c61ba5c0f65d229be379fffeb021899b
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-07-10 18:43:04 -0300

    fix: get /tickets/:id service, add exclude to attributes

--- services/ticketService.ts
+++ services/ticketService.ts
@@ -2,16 +2,15 @@ const { Ticket } = require('../models');
 
 export default class TicketService {
   static async getTicketById(id: string) {
-    const ticket = await Ticket.findByPk(id);
+    const ticket = await Ticket.findByPk(id, { attributes: { exclude: ['hash'] } });
     if (ticket === null) {
       return false;
     }
-    const { event_id, owner_id, status } = ticket;
-    return { event_id, owner_id, status }
+    return ticket;
   }
 
   static async getTicketsByOwnerId(id: string) {
-    const tickets = await Ticket.findAll({ where: { owner_id: id }, attributes: { exclude: ['hash'] }});
+    const tickets = await Ticket.findAll({ where: { owner_id: id }, attributes: { exclude: ['hash'] } });
     console.log(tickets);
     if (tickets.length === 0) {
       return false;
